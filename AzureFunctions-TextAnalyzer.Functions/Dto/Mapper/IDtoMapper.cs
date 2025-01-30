3fbbba44113157554395dfb2871fc0a0ce6a558f
Aras Rasouli <a.rasouli@gmail.com>
2025-01-30 22:49:07 +0100

    feat: create mapper for model and DTO

---
+++ AzureFunctions-TextAnalyzer.Functions/Dto/Mapper/IDtoMapper.cs
@@ -0,0 +1,9 @@
+﻿namespace AzureFunctions_TextAnalyzer.Functions.Dto.Mapper
+{
+    public interface IDtoMapper<TModel, TDto>
+    {
+        TModel MapToModel(TDto dto);
+
+        TDto MapFromModel(TModel model);
+    }
+}
--- AzureFunctions-TextAnalyzer.Functions/Program.cs
+++ AzureFunctions-TextAnalyzer.Functions/Program.cs
@@ -1,4 +1,7 @@
+using AzureFunctions_TextAnalyzer.Functions.Dto.Mapper;
+using AzureFunctions_TextAnalyzer.Functions.Dto;
 using AzureFunctions_TextAnalyzer.Service;
+using AzureFunctions_TextAnalyzer.Service.Model;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -6,6 +9,7 @@ using Microsoft.Extensions.Hosting;
 var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<IChunkServices, ChunkServices>();
+builder.Services.AddSingleton<IDtoMapper<ChunkDataModel, ChunkQueueMessageDto>, ChunkDtoMapper>();
 
 builder.ConfigureFunctionsWebApplication();
 
