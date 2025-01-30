605bd42c20eadf4b76fc0551f74f22f8642a2063
Aras Rasouli <a.rasouli@gmail.com>
2025-01-30 22:44:51 +0100

    refactor: modify model properties

--- AzureFunctions-TextAnalyzer.Service/Model/ChunkDataModel.cs
+++ AzureFunctions-TextAnalyzer.Service/Model/ChunkDataModel.cs
@@ -2,15 +2,10 @@
 {
     public class ChunkDataModel
     {
+        public string Name { get; set; }
         public long StartPoint { get; set; }
         public long EndPoint { get; set; }
         public int ChunkIndex { get; set; }
         public int ChunksCount { get; set; }
-    }
-
-    public class ChunkProcessingModel
-    {
-        public Dictionary<string, int> WordCounts { get; set; }
-        public int ChunkIndex { get; set; }
     }
 }
