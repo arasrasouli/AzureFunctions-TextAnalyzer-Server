386133c734567dd38a25991dfe681f7ca1916651
Aras Rasouli <a.rasouli@gmail.com>
2025-02-04 01:00:57 +0100

    refactor(Common): define a new enum for FileProcessingStatus

---
+++ AzureFunctions-TextAnalyzer.Common/Enum/FileProcessingStatusEnum.cs
@@ -0,0 +1,11 @@
+﻿namespace AzureFunctions_TextAnalyzer.Common.Enums
+{
+    public enum FileProcessingStatus
+    {
+        NotStarted = 0,
+        InProgress = 1,
+        Completed = 2,
+        Failed = 3,
+        Queued = 4
+    }
+}
