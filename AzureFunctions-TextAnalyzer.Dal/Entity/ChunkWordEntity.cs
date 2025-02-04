76fbd5eff14cc0b4f0c71b54480586943aeffd96
Aras Rasouli <a.rasouli@gmail.com>
2025-02-04 01:04:12 +0100

    feat(DAL): extend repositories for fileChunk and chunkWords Entities

---
+++ AzureFunctions-TextAnalyzer.Dal/Entity/ChunkWordEntity.cs
@@ -0,0 +1,25 @@
+﻿using Azure;
+using Azure.Data.Tables;
+using System.Text.Json;
+
+namespace AzureFunctions_TextAnalyzer.DAL.Entities
+{
+    public class ChunkWordEntity : ITableEntity
+    {
+        public ChunkWordEntity() { }
+
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public string WordsCount { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+
+
+        public ChunkWordEntity(string partitionKey, string rowKey, string wordsCount)
+        {
+            PartitionKey = partitionKey;
+            RowKey = rowKey;
+            WordsCount = wordsCount;
+        }
+    }
+}
---
+++ AzureFunctions-TextAnalyzer.Dal/Repository/Interfaces/IChunkWordRepository.cs
@@ -0,0 +1,8 @@
+﻿using AzureFunctions_TextAnalyzer.DAL.Entities;
+
+namespace AzureFunctions_TextAnalyzer.DAL.Repositories
+{
+    public interface IChunkWordRepository : ITableStorageRepository<ChunkWordEntity>
+    {
+    }
+}
