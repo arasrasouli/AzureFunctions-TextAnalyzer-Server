049f2c31a327630726d9f90b639ed7f2954789b1
Aras Rasouli <a.rasouli@gmail.com>
2025-02-10 02:57:10 +0100

    feat(service bus): Add Queue Names

---
+++ AzureFunctions-TextAnalyzer.Infrastructure/ServiceBus/ServiceBusQueues.cs
@@ -0,0 +1,7 @@
+﻿namespace AzureFunctions_TextAnalyzer.Infrastructure.ServiceBus
+{
+    public static class QueueNames
+    {
+        public const string WordsCountQueue = "words-count-queue";
+    }
+}
